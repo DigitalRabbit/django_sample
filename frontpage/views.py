ea0d4d58dceb6e5183ed89bc1d9e1d8539204611
Noritaka Kodaira <tenki.koda@gmail.com>
2017-06-04 13:26:23 +0900

    Add a feature that display frontpage html

--- frontpage/views.py
+++ frontpage/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+# --- coding: utf-8 ---
 
-# Create your views here.
+"""
+All views for the frontpage application.
+"""
+from django.conf import settings
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+from .models import Jumbotron, Product
+
+
+class FrontView(TemplateView):
+    """A view class that represents a front page of site."""
+
+    template_name = 'frontpage/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(FrontView, self).get_context_data(**kwargs)
+        context['jumbotron'] = Jumbotron.objects.get(pk=1)
+        context['product_list'] = Product.objects.all()
+        return context
