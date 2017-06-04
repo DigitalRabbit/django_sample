8046c53c1047c775660f3acf82ba1d0803b768be
Noritaka Kodaira <tenki.koda@gmail.com>
2017-06-04 21:10:40 +0900

    Add Model classses and register to the admin

--- navheader/admin.py
+++ navheader/admin.py
@@ -1,3 +1,20 @@
+# --- coding: utf-8 ---
+
+"""
+Register Model classes and ModelAdmin classes to the Django admin application.
+"""
 from django.contrib import admin
 
-# Register your models here.
+from .models import HeaderItem
+
+
+class HeaderItemAdmin(admin.ModelAdmin):
+    """
+    Customaization of HeaderItem
+    """
+
+    list_display = ('item_order', 'edge', 'title_text', 'link_url')
+    list_display_links = ('title_text',)
+
+
+admin.site.register(HeaderItem, HeaderItemAdmin)
--- navheader/models.py
+++ navheader/models.py
@@ -1,3 +1,30 @@
+# --- coding: utf-8 ---
+
+"""
+All models for the navigation header application.
+"""
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class HeaderItem(models.Model):
+    """Represent a item that has title text and link url."""
+
+    class Meta:
+        ordering = ['item_order']
+
+    EDGE_CHOICE = (
+        ('left', 'left'),
+        ('right', 'right'),
+    )
+
+    item_order = models.PositiveSmallIntegerField(
+        unique=True,
+        validators=[MinValueValidator(1)],
+        default=1)
+    title_text = models.CharField(max_length=100)
+    link_url = models.URLField()
+    edge = models.CharField(max_length=10, choices=EDGE_CHOICE)
+
+    def __str__(self):
+        return self.title_text
